cae07d3cbda6c35de8836c83e5d0effb3231c210
RonaldBoyd <ronboyd@hotmail.com>
2016-06-24 23:07:00 -0400

    Test

--- site/module5-solution/js/script.js
+++ site/module5-solution/js/script.js
@@ -118,7 +118,7 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       // 
-      var homeHtmlToInsertIntoMainPage = "<h1>It Works!</h1>"
+      var homeHtmlToInsertIntoMainPage = $dc.loadMenuItems('{{chosenCategoryShortName}}');
       
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
