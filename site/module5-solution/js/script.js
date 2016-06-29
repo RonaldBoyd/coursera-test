f4057ac2ef99023be354c05356104bfef84fe757
RonaldBoyd <ronboyd@hotmail.com>
2016-06-29 09:50:56 -0400

    Test

--- site/module5-solution/js/script.js
+++ site/module5-solution/js/script.js
@@ -119,7 +119,7 @@ function buildAndShowHomeHTML (categories) {
       console.log(chosenCategoryShortName);
       homeHtmlToInsertIntoMainPage="<h1>TEST</h1>";
        console.log(homeHtmlToInsertIntoMainPage);     // 
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"{{short_name}}",chosenCategoryShortName);
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"{{randomCategoryShortName}}",chosenCategoryShortName);
       console.log(homeHtmlToInsertIntoMainPage);
 //...
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
