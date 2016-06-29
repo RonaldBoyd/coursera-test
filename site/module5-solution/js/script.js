256449a9ecd48de6c37fa30bd0b750a399a20039
RonaldBoyd <ronboyd@hotmail.com>
2016-06-29 09:14:27 -0400

    Test

--- site/module5-solution/js/script.js
+++ site/module5-solution/js/script.js
@@ -118,7 +118,7 @@ function buildAndShowHomeHTML (categories) {
       console.log("TESTING");
       console.log(chosenCategoryShortName);
       // 
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"{{short_name}}",chosenCategoryShortName);//...
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"short_name",chosenCategoryShortName);//...
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that. 
